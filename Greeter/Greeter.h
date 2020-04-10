c67172733c46fbed73d893a837f7d13490fbd022
Dan Wallace <daniel.wallace.nz@gmail.com>
2020-04-10 16:29:34 +1200

    Initial Commit

---
+++ Greeter/Greeter.h
@@ -0,0 +1,19 @@
+//
+//  Greeter.h
+//  Greeter
+//
+//  Created by Daniel Wallace on 10/04/20.
+//  Copyright © 2020 danwallacenz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Greeter.
+FOUNDATION_EXPORT double GreeterVersionNumber;
+
+//! Project version string for Greeter.
+FOUNDATION_EXPORT const unsigned char GreeterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Greeter/PublicHeader.h>
+
+
