de2ff85f61e3c495aed521c8d9c879e22a438735
Dan Wallace <daniel.wallace.nz@gmail.com>
2020-04-10 16:34:25 +1200

    add GreetingProvider

---
+++ Greeter/GreetingProvider.swift
@@ -0,0 +1,16 @@
+//
+//  GreetingProvider.swift
+//  Greeter
+//
+//  Created by Daniel Wallace on 10/04/20.
+//  Copyright © 2020 danwallacenz. All rights reserved.
+//
+
+import Foundation
+
+public class GreetingProvider {
+   
+    public func greet(withPrefix prefix: String) -> String {
+        return prefix + " Hello!"
+    }
+}
